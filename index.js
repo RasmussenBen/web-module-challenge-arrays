99fb47a8a334c11fe0470c75824831a7b836b518
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-04-14 14:03:56 -0500

    fixed task 2

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 
 function is31Flavors(array){
    if (array.length === 31) {
-       true;
+       return true;
    } else {
-       false;
+       return false;
    }
 }
 
